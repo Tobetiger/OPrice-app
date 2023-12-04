93b6b93a748b9e142d82e1beab4f7989c1d8b4b7
Emmanuel Alexander <iamtobeconfidential@gmail.com>
2023-12-04 12:59:57 +0100

    refactor: made some new changes on the nav and product page

--- app/productsPage/[id]/page.tsx
+++ app/productsPage/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getProductById } from "@/mainLib/actions";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 type Props = {
@@ -10,7 +11,21 @@ const productsPage = async ({ params: { id } }: Props) => {
 
   if (!product) redirect("/");
 
-  return <div>{id}</div>;
+  return (
+    <div className="flex flex-col gap-16 flex-wrap px-6 md:px-20 py-24">
+      <div>
+        <div>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={580}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default productsPage;
