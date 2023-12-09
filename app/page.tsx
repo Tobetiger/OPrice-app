151c7323f7e6567ac04eb0338cd9f183c55c4bfd
Emmanuel Alexander <iamtobeconfidential@gmail.com>
2023-12-09 12:14:08 +0100

    refactor: readded some features to home page

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,21 @@ const Home = async () => {
         </div>
       </MaxWidthWrapper>
 
+      <section
+        id="trending"
+        className="flex flex-col gap-10 px-6 md:px-20 py-24 mx-auto"
+      >
+        <h2 className="mt-2 -mb-6 font-bold text-2xl text-gray-900 sm:text-2xl">
+          All Recently Tracked Products
+        </h2>
+        <hr className="" />
+        <div className="flex text-black flex-wrap gap-x-14 gap-y-16">
+          {allProducts?.reverse().map((product) => (
+            <ProductCards key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+
       <div>
         <div className="relative-isolate">
           <div
